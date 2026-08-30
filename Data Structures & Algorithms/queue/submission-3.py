82337723a5d02e0b6b5bba13c9107ec99ab931d3
Zura16 <kaleaalind7@gmail.com>
2026-08-29 19:53:46 -0700

    Add: queue - submission-3

---
+++ Data Structures & Algorithms/queue/submission-3.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
+
+class Deque:
+    
+    def __init__(self):
+        self.head = Node(0)
+        self.tail = Node(0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def isEmpty(self) -> bool:
+        return self.head.next == self.tail
+
+    def append(self, value: int) -> None:
+        node, next, prev = Node(value), self.tail, self.tail.prev
+        prev.next = node
+        next.prev = node
+        node.next = next
+        node.prev = prev
+
+    def appendleft(self, value: int) -> None:
+        node, next, prev = Node(value), self.head.next, self.head
+        prev.next = node
+        next.prev = node
+        node.next = next
+        node.prev = prev
+
+    def pop(self) -> int:
+        if self.isEmpty():
+            return -1
+        node = self.tail.prev
+        next = self.tail
+        prev = node.prev
+        prev.next = next
+        next.prev = prev
+        return node.val
+
+    def popleft(self) -> int:
+        if self.isEmpty():
+            return -1
+        node = self.head.next
+        next = node.next
+        prev = self.head
+        prev.next = next
+        next.prev = prev
+        return node.val
