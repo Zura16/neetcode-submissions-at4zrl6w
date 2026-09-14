70314aa381f2ac480a69c5cd377973687e532ebc
Zura16 <kaleaalind7@gmail.com>
2026-09-14 11:58:01 -0700

    Add: lru-cache - submission-2

---
+++ Data Structures & Algorithms/lru-cache/submission-2.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, key, value): 
+        self.key, self.value = key, value
+        self.next = self.prev = None 
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.hm = {} 
+        self.right, self.left = Node(0, 0), Node(0, 0)
+        self.right.prev, self.left.next = self.left, self.right
+
+    def remove(self, node):
+        next, prev = node.next, node.prev
+        prev.next, next.prev = next, prev
+
+    def insert(self, node):
+        next, prev = self.right, self.right.prev
+        prev.next = next.prev = node
+        node.next, node.prev = next, prev
+
+    def get(self, key: int) -> int:
+        if key in self.hm:
+            self.remove(self.hm[key])
+            self.insert(self.hm[key])
+            return self.hm[key].value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.hm:
+            self.remove(self.hm[key])
+        self.hm[key] = Node(key, value)
+        self.insert(self.hm[key])
+        if len(self.hm) > self.capacity:
+            lru = self.left.next
+            self.remove(lru)
+            del self.hm[lru.key]
